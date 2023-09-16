24a8beec4aa08028025c4dda1e7c7288a1c3214e
sliponsandpaper <xkleong.2022@smu.edu.sg>
2023-09-16 22:00:51 +0800

    last save

--- express/app.js
+++ express/app.js
@@ -3,6 +3,21 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const { MongoClient } = require('mongodb');
+const connectionString = 'mongodb://localhost:27017/CraftWillsTest'; // Replace with your connection string
+const client = new MongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToMongo() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+}
+
+
+
 const indexRouter = require('./index'); // cannot use import because in index we dont have export so cannot
 // Load environment variables from a .env file
 dotenv.config();
@@ -19,7 +34,6 @@ app.use(cookieParser()); // pass user cookies
 mongoose.connect(process.env.MONGO_URI, { // this means go to the env file to put the URI
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
 });
 
 const db = mongoose.connection; // connect to database
